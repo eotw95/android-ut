b9510d0c47b483384e29a0752a01e7db21e1747b
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-05-02 01:46:06 +0900

    Add anotation using Junit4 test runner

--- app/src/test/java/com/eotw95/androidutsample/InputCheckerTest.kt
+++ app/src/test/java/com/eotw95/androidutsample/InputCheckerTest.kt
@@ -6,7 +6,10 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(JUnit4::class)
 class InputCheckerTest {
     private lateinit var inputChecker: InputChecker
 
